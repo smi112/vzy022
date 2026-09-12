b06154aa8f24f0a3df4d0aa67d81d80c4224eb40
smi112 <kapidobrykapicz@gmail.com>
2026-09-12 15:24:48 +0200

    Naprawiono onclick nawigację - przechwytywanie wszystkich kliknięć w standalone

--- js/hide-navbar.js
+++ js/hide-navbar.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function() {
     var isStandalone = window.navigator.standalone || window.matchMedia('(display-mode: standalone)').matches;
     
     if (isStandalone) {
-        console.log("Aplikacja działa w trybie standalone - linki będą otwierane wewnątrz aplikacji");
+        console.log("Aplikacja działa w trybie standalone - przechwytywanie nawigacji");
         
         // Przechwytuj wszystkie kliknięcia w linki <a>
         var links = document.getElementsByTagName("a");
@@ -15,23 +15,53 @@ window.addEventListener("load", function() {
                 // Jeśli to link wewnętrzny (nie zaczyna się od http://, https://, mailto:, tel:, #)
                 if (href && href.indexOf('http') !== 0 && href.indexOf('mailto:') !== 0 && href.indexOf('tel:') !== 0 && href !== '#') {
                     event.preventDefault();
+                    event.stopPropagation();
                     window.location.href = href;
+                    return false;
                 }
-            }, false);
+            }, true); // useCapture = true
         }
         
-        // Obsługa onclick="window.location.href='...'" - nadpisz window.location.href
-        var originalLocationSetter = Object.getOwnPropertyDescriptor(window.Location.prototype, 'href').set;
-        Object.defineProperty(window.location, 'href', {
-            set: function(url) {
-                // Użyj oryginalnej funkcji ale w trybie standalone
-                if (url && url.indexOf('http') !== 0) {
-                    originalLocationSetter.call(window.location, url);
-                } else {
-                    originalLocationSetter.call(window.location, url);
+        // Nadpisz window.location.href aby działało w standalone
+        var originalPushState = history.pushState;
+        var originalReplaceState = history.replaceState;
+        
+        // Przechwytuj wszystkie zmiany window.location poprzez delegację zdarzeń
+        document.addEventListener('click', function(e) {
+            var target = e.target;
+            var maxDepth = 5; // Sprawdź max 5 poziomów w górę
+            
+            // Sprawdź czy kliknięto element lub jego rodzica z onclick
+            while (target && maxDepth > 0) {
+                var onclickAttr = target.getAttribute('onclick');
+                
+                if (onclickAttr && onclickAttr.indexOf('window.location') !== -1) {
+                    // Wyciągnij URL z onclick="window.location.href='url'"
+                    var match = onclickAttr.match(/['"](.*?)['"]/);
+                    if (match && match[1]) {
+                        var url = match[1];
+                        
+                        // Jeśli to wewnętrzny link HTML
+                        if (url.indexOf('.html') !== -1 || url.indexOf('#') === 0) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            
+                            console.log("Przechwycono onclick nawigację do:", url);
+                            
+                            // Użyj setTimeout aby uniknąć problemów z event loop
+                            setTimeout(function() {
+                                window.location.href = url;
+                            }, 0);
+                            
+                            return false;
+                        }
+                    }
                 }
+                
+                target = target.parentElement;
+                maxDepth--;
             }
-        });
+        }, true); // useCapture = true - przechwytuj przed innymi handlerami
     }
     
     // Alternatywne podejście do ukrywania paska adresu (dla starszych wersji)
